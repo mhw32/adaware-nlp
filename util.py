de3caf48983b655216c6928ea047302d29d21ea0
Jesse <jesseyli24@gmail.com>
2016-10-14 20:15:46 -0700

    POS tagger

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import numpy as np
 
 def is_int(x):
     try:
@@ -13,3 +14,20 @@ def FreqDist(tags):
     for tag in tags:
         freq[tag] += 1
     return freq
+
+def batch_index_generator(n, batch_size=50):
+    i = 0
+    shuffled_indices = np.random.permutation(n)
+    while True:
+        if i + batch_size > n:
+            shuffled_indices = np.random.permutation(n)
+            i = 0
+        yield shuffled_indices[i:i+batch_size]
+        i += batch_size
+
+def sigmoid(x):
+    return 0.5*(np.tanh(x) + 1.0)
+
+def concat_and_multiply(weights, *args):
+    cat_state = np.hstack(args + (np.ones((args[0].shape[0], 1)),))
+    return np.dot(cat_state, weights)
