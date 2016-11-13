a6dbc073b399714b53de2d8b24d8ea2425bac7e1
mhw32 <me@mikewuis.me>
2016-11-13 00:52:22 -0800

    finished -- the adanlp app works

--- api_demo/app/views.py
+++ api_demo/app/views.py
@@ -3,15 +3,17 @@ from app import app
 
 import sys
 import os
+import json
+from datetime import datetime
 local_ref = lambda x: os.path.join(os.path.dirname(__file__),  x)
 sys.path.append(local_ref('../../core'))
 from bank import ParamBank
-from pipeline import AdaSentencePipeline
+from pipeline import AdaTextPipeline
 
 
 pbank = ParamBank()
 pbank.load()
-pipe = AdaSentencePipeline(pbank)
+pipe = AdaTextPipeline(pbank)
 
 
 @app.route('/', methods = ['POST', 'GET'])
@@ -22,5 +24,10 @@ def index():
 @app.route('/evaluate_text', methods=['GET'])
 def evaluate_text():
     input_str = request.args.get('input_str', None, type=str)
-    res_json = pipe.do(input_str) if not input_str is None else 'None'
-    return jsonify(result=res_json)
+    print('[{}] Retrieved input: {}'.format(str(datetime.now()), input_str))
+    print('[{}] Executing Ada Pipeline'.format(str(datetime.now())))
+    if not input_str is None:
+        res_json = pipe.do(input_str)
+        print('[{}] Returning JSON: {}'.format(str(datetime.now()), res_json))
+        return jsonify(result=json.dumps(res_json))
+    return jsonify(result='None')
--- core/pipeline.py
+++ core/pipeline.py
@@ -22,6 +22,7 @@ class AdaTextPipeline(object):
         disambiguator_tag_counts = bank.get('disambiguator_tag_counts')
         disambiguator_tag_order = bank.get('disambiguator_tag_order')
 
+        print('[{}] Running Disambiguator'.format(str(datetime.now())))
         self.disambiguator = nlp.AdaSentenceDisambiguator(
             disambiguator_weights, disambiguator_tag_counts, disambiguator_tag_order)
 
@@ -65,11 +66,17 @@ class AdaSentencePipeline(object):
         self.dep_parser = nlp.AdaDependencyParser(dep_path_to_jar, dep_path_to_models_jar)
 
     def do(self, sentence):
+        print('[{}] Running Tokenizer'.format(str(datetime.now())))
         tokens = self.tokenizer.do(sentence)
+        print('[{}] Running Lemmatizer'.format(str(datetime.now())))
         lemmas = self.lemmatizer.do_all(tokens)
-        embeddings = self.embedder.do_all(tokens)
+        print('[{}] Running Embedder'.format(str(datetime.now())))
+        embeddings = [i for i in self.embedder.do_all(tokens)]
+        print('[{}] Running POS Tagger'.format(str(datetime.now())))
         postags = self.pos_tagger.do(tokens)
+        print('[{}] Running NER Classifier'.format(str(datetime.now())))
         nertags = self.ner_classifier.do(tokens)
+        print('[{}] Running Dependency Parsing'.format(str(datetime.now())))
         deptags = self.dep_parser.do(tokens)
 
         resp = { 'tokens' : tokens,
@@ -79,12 +86,18 @@ class AdaSentencePipeline(object):
                  'ner_tags' : nertags,
                  'dep_tags' : deptags,
                  '_timestamp' : str(datetime.now()) }
+        return resp
 
     def _do_tokens(self, tokens):
+        print('[{}] Running Lemmatizer'.format(str(datetime.now())))
         lemmas = self.lemmatizer.do_all(tokens)
-        embeddings = self.embedder.do_all(tokens)
+        print('[{}] Running Embedder'.format(str(datetime.now())))
+        embeddings = [i for i in self.embedder.do_all(tokens)]
+        print('[{}] Running POS Tagger'.format(str(datetime.now())))
         postags = self.pos_tagger.do(tokens)
+        print('[{}] Running NER Classifier'.format(str(datetime.now())))
         nertags = self.ner_classifier.do(tokens)
+        print('[{}] Running Dependency Parsing'.format(str(datetime.now())))
         deptags = self.dep_parser.do(tokens)
 
         resp = { 'tokens' : tokens,
@@ -94,7 +107,7 @@ class AdaSentencePipeline(object):
                  'ner_tags' : nertags,
                  'dep_tags' : deptags,
                  '_timestamp' : str(datetime.now()) }
-
+        return resp
 
 class AdaWordPipeline(object):
     def __init__(self):
@@ -107,7 +120,9 @@ class AdaWordPipeline(object):
         embedder_weights = bank.get('embedder_weights')
         embedder_vocab = bank.get('embedder_vocab')
 
+        print('[{}] Running Lemmatizer'.format(str(datetime.now())))
         self.lemmatizer = nlp.AdaLemmatizer()
+        print('[{}] Running Embedder'.format(str(datetime.now())))
         self.embedder = nlp.AdaWordEmbedder(embedder_weights, embedder_vocab)
 
     def do(self, word):
