5afec5d60f0e856af87e9e74a63fb9b0b7c2b914
mhw32 <me@mikewuis.me>
2016-11-12 21:10:23 -0800

    added a demo for adanlp demo

---
+++ api_demo/app/views.py
@@ -0,0 +1,26 @@
+from flask import render_template, request, jsonify
+from app import app
+
+import sys
+import os
+local_ref = lambda x: os.path.join(os.path.dirname(__file__),  x)
+sys.path.append(local_ref('../../core'))
+from bank import ParamBank
+from pipeline import AdaSentencePipeline
+
+
+pbank = ParamBank()
+pbank.load()
+pipe = AdaSentencePipeline(pbank)
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/evaluate_text', methods=['GET'])
+def evaluate_text():
+    input_str = request.args.get('input_str', 'None', type=str)
+    res_json = pipe.do(input_str)
+    return jsonify(result=res_json)
